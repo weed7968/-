de24cda50a4b575cefb13739f177d20bb513fede
weed7968 <tlstjdcjf980809@gmail.com>
2024-01-28 19:15:21 +0900

    24-01-28

---
+++ lv2/뒤에 있는 큰 수 찾기.js
@@ -0,0 +1,13 @@
+function solution(numbers) {
+  const result = new Array(numbers.length).fill(-1);
+  const stack = [];
+  for (let i = 0; i < numbers.length; i++) {
+    while (numbers[i] > numbers[stack.at(-1)]) {
+      const index = stack.pop();
+      result[index] = numbers[i];
+    }
+
+    stack.push(i);
+  }
+  return result;
+}
