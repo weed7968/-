08cdfbc10f2a46535408809c0ae4746dfbc35d3e
weed7968 <tlstjdcjf980809@gmail.com>
2024-03-08 21:59:10 +0900

    24-03-08

---
+++ re/lv2/영어 끝말잇기.js
@@ -0,0 +1,18 @@
+function solution(n, words) {
+  var answer = [];
+  let usedWords = new Set();
+  let last = words[0][words[0].length - 1];
+
+  usedWords.add(words[0]);
+
+  for (let i = 1; i < words.length; i++) {
+    if (last !== words[i][0] || usedWords.has(words[i])) {
+      answer.push((i % n) + 1);
+      answer.push(Math.floor(i / n) + 1);
+      break;
+    }
+    last = words[i][words[i].length - 1];
+    usedWords.add(words[i]);
+  }
+  return answer.length === 0 ? [0, 0] : answer;
+}
