b2e29e15e7f5fffa23c3124404d994897eb8d655
weed7968 <tlstjdcjf980809@gmail.com>
2024-05-12 16:44:41 +0900

    24-05-12

---
+++ lv1/로또의 최고 순위와 최저 순위.js
@@ -0,0 +1,10 @@
+function solution(lottos, win_nums) {
+  const correct = lottos.filter((lotto) => win_nums.includes(lotto)).length;
+
+  const zeros = lottos.filter((lotto) => lotto === 0).length;
+
+  let min = 7 - correct >= 6 ? 6 : 7 - correct;
+  let max = min - zeros < 1 ? 1 : min - zeros;
+
+  return [max, min];
+}
