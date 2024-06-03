3c9ade6347f8dff3c0ec0dad46da40f716e979cf
weed7968 <tlstjdcjf980809@gmail.com>
2024-06-03 22:11:41 +0900

    24-06-03

---
+++ lv1/완주하지 못한 선수.js
@@ -0,0 +1,24 @@
+function solution(participant, completion) {
+  participant.sort();
+  completion.sort();
+  for (var i = 0; i < participant.length; i++) {
+    if (participant[i] !== completion[i]) {
+      return participant[i];
+    }
+  }
+}
+
+function solution(participant, completion) {
+  const map = new Map();
+
+  for (let i = 0; i < participant.length; i++) {
+    let a = participant[i];
+    let b = completion[i];
+    map.set(a, (map.get(a) || 0) + 1);
+    map.set(b, (map.get(b) || 0) - 1);
+  }
+  for (let [k, v] of map) {
+    if (v > 0) return k;
+  }
+  return "nothing";
+}
