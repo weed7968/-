bc429983128cd05e40aa7e81ec0a555959686f58
weed7968 <tlstjdcjf980809@gmail.com>
2024-07-01 23:11:35 +0900

    24-07-01

---
+++ re/lv1/비밀지도.js
@@ -0,0 +1,8 @@
+function solution(n, arr1, arr2) {
+  let arr = arr1.map((obj, idx) => {
+    let sum = Number(obj.toString(2)) + Number(arr2[idx].toString(2));
+    return "0".repeat(n - sum.toString().length) + sum.toString().replaceAll("2", "1");
+  });
+
+  return arr.map((obj) => obj.replaceAll("1", "#").replaceAll("0", " "));
+}
